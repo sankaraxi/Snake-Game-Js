b9bbaac4b24176726c35a86fe1eb09ce409ea5fb
sankaraxi <sankarfrompalani@gmail.com>
2024-07-24 02:14:46 +0530

    added some javascript features

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const board = document.getElementById('game-board');
 
 //Defining game variables
-let snake = [{x: 10, y: 10}];
+let snake = [{x: 20, y: 20}];
 
 //Draw game map, snake and food
 function draw(){
@@ -12,8 +12,26 @@ function draw(){
 
 //Draw the snake
 function drawSnake(){
-    snake.forEach(segment => {
-        const snakeElement = document.createElement('div','snake');
-
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div','snake');
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
     });
 }
+
+//Create a snake or food cube/div
+function createGameElement(tagName, className){
+    const element = document.createElement(tagName);
+    element.className = className; //Add class to the element
+    return element;
+}
+
+
+// Set the position of the snake or food
+function setPosition(element, position){
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+//Test the draw function
+// draw();
